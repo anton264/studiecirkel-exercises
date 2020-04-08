21adffcbc6f228e2890b25c0d80be4d8da4f8c84
Anton Eriksmo <anton.eriksmo@evry.com>
2020-04-08 10:50:47 +0200

    isogram

--- week3/src/main/kotlin/com/hackosynth/week3/isogram/Isogram.kt
+++ week3/src/main/kotlin/com/hackosynth/week3/isogram/Isogram.kt
@@ -1,5 +1,11 @@
 package com.hackosynth.week3.isogram
 
 object Isogram {
-    fun isIsogram(input: String): Boolean = TODO()
+    fun isIsogram(input: String): Boolean {
+        val cleanInput = Regex("[^a-zA-Z]").replace(input, "").toLowerCase()
+        for (char in cleanInput) {
+            if (cleanInput.count { char.toString().contains(it) } >= 2) return false
+        }
+    return true
+    }
 }
