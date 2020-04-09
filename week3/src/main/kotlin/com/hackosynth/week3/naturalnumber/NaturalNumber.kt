7ff2ac893efb7b40f5ce4d74afb93a01be0c570c
Anton Eriksmo <anton.eriksmo@evry.com>
2020-04-09 10:00:08 +0200

    Naturalnumber

--- week3/src/main/kotlin/com/hackosynth/week3/naturalnumber/NaturalNumber.kt
+++ week3/src/main/kotlin/com/hackosynth/week3/naturalnumber/NaturalNumber.kt
@@ -10,14 +10,22 @@ enum class Classification {
 object NaturalNumber {
     fun classify(naturalNumber: Int): Classification {
         if (naturalNumber < 1) throw RuntimeException("$naturalNumber is not a valid number")
+        val sumOfDivisors =  (1 until naturalNumber).filter { naturalNumber % it == 0 }.sum()
+
+        //This "oldschool" solution is twice as fast as the one above, why?
+        /*
         val list = mutableListOf<Int>()
         for (number in 1 until naturalNumber.minus(1)) {
             if (naturalNumber % number == 0) list.add(number)
         }
+        val sumOfDivisors = list.sum()
+        */
+
+
         return when {
-            list.sum() == naturalNumber -> Classification.PERFECT
-            list.sum() > naturalNumber -> Classification.ABUNDANT
-            list.sum() < naturalNumber -> Classification.DEFICIENT
+            sumOfDivisors == naturalNumber -> Classification.PERFECT
+            sumOfDivisors > naturalNumber -> Classification.ABUNDANT
+            sumOfDivisors < naturalNumber -> Classification.DEFICIENT
             else -> error("Ett fel är trasigt")
 
         }
